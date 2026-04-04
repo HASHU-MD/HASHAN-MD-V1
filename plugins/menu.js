31a5c13f70f5bf0e2b680e21cd613405a66c20e2
HASHU-MD <dinujavirajitha364@gmail.com>
2026-04-04 23:45:00 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -16,27 +16,27 @@ cmd({
   try {
     // Menu Text
     const menuText = `
-╭═════════════════⚆
-│ *Creator* : ᴍʀ ʜᴀꜱʜᴜᴜ
-│ *Version* : v0.1
-│ *Uptime*  : ${runtime(process.uptime())}
-│ *RAM Usage* : ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB
-│ *Host Name* : ${require('os').hostname()}
-╰═════════════════⚆
+  ╭═════════════════⚆
+> │ *ᴄʀᴇᴀᴛᴏʀ* : ᴍʀ ʜᴀꜱʜᴜᴜ
+> │ *ᴠᴇʀꜱɪᴏɴ* : v0.1
+> │ *ᴜᴘᴛɪᴍᴇ*  : ${runtime(process.uptime())}
+> │ *ʀᴀᴍ ᴜꜱᴀɢᴇ* : ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB
+> │ *ʜᴏꜱᴛ ɴᴀᴍᴇ* : ${require('os').hostname()}
+  ╰═════════════════⚆
 │💮 *LIST MENU*
 │   ───────
-│ _1_ *❂ ᴅᴏᴡɴʟᴏᴀᴅ menu❂*
-│ _2_ *❂ ᴏᴡɴᴇʀ menu❂*
-│ _3_ *❂ ɢʀᴏᴜᴘ ᴍᴇɴᴜ❂*
-│ _4_ *❂ ᴄᴏɴᴠᴇʀᴛ menu❂*
-│ _5_ *❂ ᴀɪ ᴍᴇɴᴜ❂*
-│ _6_ *❂ ꜱᴇᴀʀᴄʜ menu❂*
-│ _7_ *❂ ꜰᴜɴ menu❂*
-│ _8_ *❂ ʙᴜɢ menu❂*
+│ _1️⃣_ *❂ ᴅᴏᴡɴʟᴏᴀᴅ menu❂*
+│ _2️⃣_ *❂ ᴏᴡɴᴇʀ menu❂*
+│ _3️⃣_ *❂ ɢʀᴏᴜᴘ ᴍᴇɴᴜ❂*
+│ _4️⃣_ *❂ ᴄᴏɴᴠᴇʀᴛ menu❂*
+│ _5️⃣_ *❂ ᴀɪ ᴍᴇɴᴜ❂*
+│ _6️⃣_ *❂ ꜱᴇᴀʀᴄʜ menu❂*
+│ _7️⃣_ *❂ ꜰᴜɴ menu❂*
+│ _8️⃣_ *❂ ʙᴜɢ menu❂*
 ╰═════════════════⚆
 *🌟 Reply the Number you want to select*
 
-> © ᴘᴏᴡᴇʀᴇᴅ ʙʏ ʜᴀꜱʜᴀɴ-ᴍᴅ 💮`;
+> © ᴘᴏᴡᴇʀᴇᴅ ʙʏ ʜᴀꜱʜᴀɴ-ᴍᴅ 😚💗`;
 
     // Send Menu Message
     const sentMenuMessage = await bot.sendMessage(from, {
