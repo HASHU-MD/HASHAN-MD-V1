9e5a00d203227de813cb46523acb5604169e3146
HASHU-MD <dinujavirajitha364@gmail.com>
2026-04-04 23:30:09 +0530

    Update image URL in alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -34,7 +34,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 
 
         await conn.sendMessage(from, { 
-            image: { url: `https://o.uguu.se/JoMMGxeG.jpg` },  // Image URL
+            image: { url: `https://files.catbox.moe/vbo0vq.png` },  // Image URL
             caption: status,
             contextInfo: {
                 mentionedJid: [m.sender],
