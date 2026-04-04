761577817ce045f31540b902dd41835f0729df97
HASHU-MD <dinujavirajitha364@gmail.com>
2026-04-05 00:34:49 +0530

    Update ALIVE_IMG default value in config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "XlJzzCgJ#o5pmxs1-S-S5UnsgRSs0rNRlRTPydViA3H1pi0hnaGY",
-ALIVE_IMG: process.env.ALIVE_IMG || "Enter The Image URL",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/vbo0vq.png",
 AUTO_REPLY: process.env.AUTO_REPLY || "true",
 ALIVE_MSG: process.env.ALIVE_MSG || "HELLO IM HASHAN MD CREATED BY HASHAN <NOW ALIVE> ",
 };
