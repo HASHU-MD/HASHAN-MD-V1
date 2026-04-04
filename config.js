94e0b19058f680c182a298dc900aab9623d605c2
HASHU-MD <dinujavirajitha364@gmail.com>
2026-04-05 00:11:57 +0530

    Add AUTO_REPLY configuration option

--- config.js
+++ config.js
@@ -7,5 +7,6 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "XlJzzCgJ#o5pmxs1-S-S5UnsgRSs0rNRlRTPydViA3H1pi0hnaGY",
 ALIVE_IMG: process.env.ALIVE_IMG || "Enter The Image URL",
+AUTO_REPLY: process.env.AUTO_REPLY || "true",
 ALIVE_MSG: process.env.ALIVE_MSG || "HELLO IM HASHAN MD CREATED BY HASHAN <NOW ALIVE> ",
 };
