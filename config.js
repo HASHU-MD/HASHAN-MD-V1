02665df3e5f3ab6c0ffdc6c8917865751cefd953
HASHU-MD <dinujavirajitha364@gmail.com>
2026-04-08 23:51:31 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ns5GxCbJ#P1WS4SUUVI0nDGEe8o3o751AM2w9isA7Ru3X1MBLPhY",
+SESSION_ID: process.env.SESSION_ID || "OdA02Yja#2pH97b1FSY4kWBOrLt3vjLXdoHn9jUVNNOSCxIIN6fE",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/vbo0vq.png",
 AUTO_REPLY: process.env.AUTO_REPLY || "true",
 ALIVE_MSG: process.env.ALIVE_MSG || "HELLO IM HASHAN MD CREATED BY HASHAN <NOW ALIVE> ",
